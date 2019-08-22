63c7f1bb2cbc951989f83ae7793ec1475da3f176
Hurlyburly2 <dougdougmann@gmail.com>
2019-08-22 15:20:10 -0400

    fixed nav bar- notes functionality no longer appears when not logged in

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -13,11 +13,13 @@
         exact
         to="/">Home</router-link>
       <router-link
-        exact
-        to="/notes">My Notes</router-link>
-      <router-link
+        v-if="isAuthenticated"
         exact
         to="/newnote">New Note</router-link>
+      <router-link
+        v-if="isAuthenticated"
+        exact
+        to="/notes">My Notes</router-link>
       <router-link
         exact
         to="/about">About Us</router-link>
