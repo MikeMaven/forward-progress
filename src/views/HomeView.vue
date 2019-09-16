e6035eac65c015de19e3a7cfa7e2f1fcae82e3c1
Hurlyburly2 <dougdougmann@gmail.com>
2019-09-16 01:14:12 -0400

    landing page styling - pausing

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,18 @@
 <template>
-  <div class="row">
-    <div class="column">
-      <h1>Forward Progress</h1>
-      A web based SaaS platform for front office NFL executives.
-      <br>
-      <div>
-        <router-link
-          to="about"
-          class="button button-outline float-right">More info</router-link>
+  <div class="container" id="landingContainer">
+    <div class="span-image">
+      <img src="/public/images/field.jpg" class="landingImage">
+    </div>
+    <div class="row" id="textOverImage">
+      <div class="column">
+        <h1>Forward Progress</h1>
+        A web based SaaS platform for front office NFL executives.
+        <br>
+        <div>
+          <router-link
+            to="about"
+            class="button button-outline float-right">More info</router-link>
+        </div>
       </div>
     </div>
   </div>
@@ -21,3 +26,44 @@ export default {
   }
 };
 </script>
+
+<style>
+
+.span-image {
+  position: relative;
+  z-index: -1;
+}
+
+.landingImage {
+  position: absolute;
+  width: 100%;
+  /* height: 60vh; */
+  margin: auto;
+  object-fit: cover;
+  object-position: 0 -130px;
+}
+
+#textOverImage {
+  width: 100%;
+  margin: 0;
+}
+
+h1 {
+  font-family: paralucent, sans-serif;
+}
+
+#mainContainer {
+  width: 100%;
+  max-width: 100%;
+  padding: 0;
+  margin: 0;
+}
+
+#landingContainer {
+  width: 100%;
+  max-width: 100%;
+  padding: 0;
+  margin: 0;
+}
+
+</style>
