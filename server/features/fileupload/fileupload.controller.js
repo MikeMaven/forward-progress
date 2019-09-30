88e0f126e841f94b5022ae39aba19b3054430e2f
MikeMaven <mikemaven@gmail.com>
2019-09-30 15:10:18 -0400

    debugging

--- server/features/fileupload/fileupload.controller.js
+++ server/features/fileupload/fileupload.controller.js
@@ -3,6 +3,7 @@ const singleUpload = upload.single('image');
 
 exports.uploadPhoto = (req, res) => {
   singleUpload(req, res, function(err) {
+    console.log(req.file);
     return res.json({ imageUrl: req.file.location });
   });
 };
