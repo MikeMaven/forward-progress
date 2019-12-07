b57df8dca9062eda16b632ce3b297c84e4eaa122
Hurlyburly2 <dougdougmann@gmail.com>
2019-12-07 10:51:02 -0500

    consolidated migrations

--- server/db/migrations/20191205165056-add_subTitle_to_blogposts.js
+++ server/db/migrations/20191205165056-add_subTitle_to_blogposts.js
@@ -2,12 +2,21 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.addColumn('BlogPosts', 'subTitle', {
-      type: Sequelize.STRING
-    });
+    return Promise.all([
+      queryInterface.addColumn('BlogPosts', 'subTitle', {
+        type: Sequelize.STRING
+      }),
+      queryInterface.addColumn('BlogPosts', 'isPaid', {
+        type: Sequelize.BOOLEAN,
+        defaultValue: 'TRUE'
+      })
+    ]);
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.removeColumn('BlogPosts', 'subTitle');
+    return Promise.all([
+      queryInterface.removeColumn('BlogPosts', 'subTitle'),
+      queryInterface.removeColumn('BlogPosts', 'isPaid')
+    ]);
   }
 };
