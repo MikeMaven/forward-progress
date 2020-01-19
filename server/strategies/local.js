51d889717191df058c3345451f9c361e1a4eb79c
rancerun <lancesun22@gmail.com>
2020-01-19 17:12:23 -0500

    strategies refactor

--- server/strategies/local.js
+++ server/strategies/local.js
@@ -8,37 +8,34 @@ module.exports = new LocalStrategy(
     usernameField: 'usernameOrEmail',
     passwordField: 'password'
   },
-  (usernameOrEmail, password, done) => {
-    User.findOne({
-      include: [
-        { model: DB.Role, attributes: ['id', 'name'] },
-        { model: DB.UserImage, attributes: ['id'] }
-      ],
-      where: {
-        $or: [
-          {
-            username: {
-              $eq: usernameOrEmail.toLowerCase()
+  async (usernameOrEmail, password, done) => {
+    try {
+      const user = await User.findOne({
+        include: [
+          { model: DB.Role, attributes: ['id', 'name'] },
+          { model: DB.UserImage, attributes: ['id'] }
+        ],
+        where: {
+          $or: [
+            {
+              username: {
+                $eq: usernameOrEmail.toLowerCase()
+              }
+            },
+            {
+              email: {
+                $eq: usernameOrEmail.toLowerCase()
+              }
             }
-          },
-          {
-            email: {
-              $eq: usernameOrEmail.toLowerCase()
-            }
-          }
-        ]
-      }
-    })
-      .then(user => {
-        if (!user || !user.validPassword(password)) {
-          return done(
-            null,
-            false,
-            `Invalid username or password (${new Date().toLocaleTimeString()})`
-          );
+          ]
         }
-        return done(null, user);
-      })
-      .catch(err => done(err));
+      });
+      return user && user.validPassword(password)
+        ? done(null, user)
+        : done(null, false, `Invalid username or password (${new Date().toLocaleTimeString()})`)
+    }
+    catch(error) {
+      done(error);
+    }
   }
 )
