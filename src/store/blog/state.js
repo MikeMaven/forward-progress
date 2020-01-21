44d5cb947d51aeed21f770f8a34bb92e7ddbcc26
MikeMaven <mikemaven@gmail.com>
2020-01-21 16:40:54 -0500

    edits to dummy data

--- src/store/blog/state.js
+++ src/store/blog/state.js
@@ -5,8 +5,8 @@ export default {
   blogPosts: [
     {
       id: 1,
-      title: 'A Test Article About Football: Why We Build Things',
-      subTitle: "Let's look into our motives.",
+      title: 'Coming Soon: In Depth Articles',
+      subTitle: 'Football analysis at the next level.',
       coverImageURL:
         'https://sportshub.cbsistatic.com/i/r/2019/12/22/92c8a13e-e2e5-49ff-8232-519806daf2f7/thumbnail/1200x675/2fde3cdfbef8ad83e94a5462a99e017e/tom-brady-patriots.jpg',
       body:
@@ -16,8 +16,8 @@ export default {
     },
     {
       id: 2,
-      title: 'A Test Article About Football: The Power To Make Changes',
-      subTitle: 'Can you edit your code?',
+      title: 'Coming Soon: Subscriber Benefits',
+      subTitle: 'Stats analysis exclusive for subscribers',
       coverImageURL:
         'https://image.businessinsider.com/5c0e6be59d860a4b1b51b024?width=1100&format=jpeg&auto=webp',
       body:
@@ -27,8 +27,8 @@ export default {
     },
     {
       id: 3,
-      title: 'A Test Article About Football: The Power To Make Changes',
-      subTitle: 'Can you edit your code?',
+      title: 'Forward Progress',
+      subTitle: 'Changing the field of football.',
       coverImageURL:
         'https://sportshub.cbsistatic.com/i/r/2019/09/26/f81faf11-54b5-4c2f-9d7f-d129b9e45803/thumbnail/640x360/9848805b319ccb9c8891ee149e020ce7/college-football-general.jpg',
       body:
@@ -38,10 +38,10 @@ export default {
     },
     {
       id: 4,
-      title: 'Should Showboating Be Allowed?',
-      subTitle: 'We explore the reasons why truncating works and makes really long sentences look a lot better. This also entices the reader to click on the article and read more about it. This is more than 60 chars long.',
+      title: 'Forward Progress',
+      subTitle: 'Changing the field of football.',
       coverImageURL:
-        'https://images.saymedia-content.com/.image/MTY5ODc4ODc1NTUwMDAwMzI4/usatsi_13933060.jpg',
+        'https://sportshub.cbsistatic.com/i/r/2019/09/26/f81faf11-54b5-4c2f-9d7f-d129b9e45803/thumbnail/640x360/9848805b319ccb9c8891ee149e020ce7/college-football-general.jpg',
       body:
         "<p><strong>This is a test.</strong></p><p><br></p><p>We are here to make an impact in this industry.</p><p><br></p><p>For us, this should be only the beginning of some very impactful articles.</p><p><br></p><ol><li>Let's talk about this.</li></ol><p>\t\tOk!</p>",
       isPaid: false,
@@ -49,10 +49,10 @@ export default {
     },
     {
       id: 5,
-      title: 'A Test Article About Football: The Power To Make Changes',
-      subTitle: 'Can you edit your code?',
+      title: 'Forward Progress',
+      subTitle: 'Changing the field of football.',
       coverImageURL:
-        'https://www.mcclatchy-wires.com/incoming/3f76s4/picture239451308/alternates/LANDSCAPE_768/NFC_Championship_Packers_49ers_Football_35318.jpg',
+        'https://sportshub.cbsistatic.com/i/r/2019/09/26/f81faf11-54b5-4c2f-9d7f-d129b9e45803/thumbnail/640x360/9848805b319ccb9c8891ee149e020ce7/college-football-general.jpg',
       body:
         "<p><strong>This is a test.</strong></p><p><br></p><p>We are here to make an impact in this industry.</p><p><br></p><p>For us, this should be only the beginning of some very impactful articles.</p><p><br></p><ol><li>Let's talk about this.</li></ol><p>\t\tOk!</p>",
       isPaid: false,
@@ -60,10 +60,10 @@ export default {
     },
     {
       id: 6,
-      title: 'A Test Article About Football: The Power To Make Changes',
-      subTitle: 'Can you edit your code?',
+      title: 'Forward Progress',
+      subTitle: 'Changing the field of football.',
       coverImageURL:
-        'https://image.businessinsider.com/5c0e6be59d860a4b1b51b024?width=1100&format=jpeg&auto=webp',
+        'https://sportshub.cbsistatic.com/i/r/2019/09/26/f81faf11-54b5-4c2f-9d7f-d129b9e45803/thumbnail/640x360/9848805b319ccb9c8891ee149e020ce7/college-football-general.jpg',
       body:
         "<p><strong>This is a test.</strong></p><p><br></p><p>We are here to make an impact in this industry.</p><p><br></p><p>For us, this should be only the beginning of some very impactful articles.</p><p><br></p><ol><li>Let's talk about this.</li></ol><p>\t\tOk!</p>",
       isPaid: false,
@@ -71,10 +71,10 @@ export default {
     },
     {
       id: 7,
-      title: 'A Test Article About Football: The Power To Make Changes',
-      subTitle: 'Can you edit your code?',
+      title: 'Forward Progress',
+      subTitle: 'Changing the field of football.',
       coverImageURL:
-        'https://image.businessinsider.com/5c0e6be59d860a4b1b51b024?width=1100&format=jpeg&auto=webp',
+        'https://sportshub.cbsistatic.com/i/r/2019/09/26/f81faf11-54b5-4c2f-9d7f-d129b9e45803/thumbnail/640x360/9848805b319ccb9c8891ee149e020ce7/college-football-general.jpg',
       body:
         "<p><strong>This is a test.</strong></p><p><br></p><p>We are here to make an impact in this industry.</p><p><br></p><p>For us, this should be only the beginning of some very impactful articles.</p><p><br></p><ol><li>Let's talk about this.</li></ol><p>\t\tOk!</p>",
       isPaid: false,
